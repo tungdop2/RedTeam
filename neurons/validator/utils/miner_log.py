65a2205156f55c47f3915e4714dda5eb6267a249
toilaluan <tranthanhluan.nd@gmail.com>
2024-10-20 07:09:14 +0000

    update

--- neurons/validator/utils/miner_log.py
+++ neurons/validator/utils/miner_log.py
@@ -4,21 +4,25 @@ import datetime
 import numpy as np
 import pandas as pd
 
+
 class MinerCommit(BaseModel):
     encrypted_commit: str
     timestamp: float
     docker_hub_id: str = None
     key: str = None
-    
+
     def update(self, **kwargs):
         self.key = kwargs.get("key", self.key)
 
     def reveal(self) -> bool:
         if self.key:
             f = Fernet(self.key)
-            self.docker_hub_id = f.decrypt(self.encrypted_commit).decode().split("---")[1]
+            self.docker_hub_id = (
+                f.decrypt(self.encrypted_commit).decode().split("---")[1]
+            )
             return True
         return False
+
 
 class ChallengeRecord(BaseModel):
     point: float = 0
@@ -39,7 +43,9 @@ class MinerManager(BaseModel):
         today = datetime.datetime.now().strftime("%Y-%m-%d")
         if today in self.challenge_records:
             return
-        prev_day = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+        prev_day = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
+            "%Y-%m-%d"
+        )
         prev_day_record = self.challenge_records.get(prev_day, None)
 
         logs = pd.DataFrame(logs)
@@ -50,4 +56,10 @@ class MinerManager(BaseModel):
         best_uid = sorted_scores.index[0]
         if best_score > prev_day_record.score:
             score_diff = best_score - prev_day_record.score
-            today_record = ChallengeRecord(score=best_score, date=today, docker_hub_id=self.uids_to_commits[best_uid].docker_hub_id, point=score_diff, uid=best_uid)
+            today_record = ChallengeRecord(
+                score=best_score,
+                date=today,
+                docker_hub_id=self.uids_to_commits[best_uid].docker_hub_id,
+                point=score_diff,
+                uid=best_uid,
+            )
