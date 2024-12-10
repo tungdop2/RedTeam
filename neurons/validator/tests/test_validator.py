572ec782bcd4ae9fd70256a49f683e7d90839d6f
Michael Girnyak <michael@girnyak.com>
2024-12-10 18:47:03 +0200

    add teests

--- neurons/validator/tests/test_validator.py
+++ neurons/validator/tests/test_validator.py
@@ -3,10 +3,14 @@ from unittest.mock import patch, MagicMock
 from neurons.validator.validator import Validator
 import datetime
 
+@patch('redteam_core.validator.validator.get_config', MagicMock(return_value=MagicMock()))
 @patch('neurons.validator.validator.bt.wallet', MagicMock())
 @patch('neurons.validator.validator.bt.dendrite', MagicMock())
 def test_scoring_processed_when_time():
-    validator = Validator()
+    validator = MagicMock(Validator)
+    validator.update_miner_commit.return_value = None
+    validator.get_revealed_commits.return_value = {"challenge1": (["commit1"], [0])}
+
     validator.scoring_dates = []
 
     with patch('neurons.validator.validator.datetime') as mock_datetime:
