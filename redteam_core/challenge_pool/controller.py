c54d0e7bf52728115525da0c436e87274af67620
B. Batkhuu <batkhuu10@gmail.com>
2024-12-20 16:57:44 +0900

    feat: Add new challenge configuration for 'webui_auto' challenge. (#10)
    * feat: Add new challenge configuration for 'webui_auto' challenge.
    
    We are proposing new controller.py to web ui challenger environments.
    
    * Merge with main (#13)
    
    * Update requirements.txt
    
    * Update centralized scoring (#12)
    
    * add centralized scoring
    
    * update validator docs
    
    * update
    
    * update
    
    * update
    
    * update domain
    
    * update domain
    
    * update docs
    
    ---------
    
    Co-authored-by: vietbeu1998@gmail.com <vietbeu>
    
    * disable text detection challenge
    
    * Update default args reward app
    
    * Update time condition for centralized scoring
    
    * Update miner resync metagraph
    
    * Update controller.py
    
    * update challenge incentive weight
    
    ---------
    
    Co-authored-by: vietbeu1998@gmail.com <vietbeu>
    
    ---------
    
    Co-authored-by: vietbeu <44457232+vietbeu@users.noreply.github.com>
    Co-authored-by: vietbeu <vietbeu1998@gmail.com>

--- redteam_core/challenge_pool/controller.py
+++ redteam_core/challenge_pool/controller.py
@@ -1,13 +1,16 @@
-from typing import List, Dict
+import re
+import time
+import copy
+import subprocess
+from typing import List, Dict, Tuple, Union
+
 import docker
 import docker.types
 import requests
 import bittensor as bt
+
 from ..constants import constants
-import time
-import subprocess
-import copy
-import re
+
 
 class Controller:
     """
@@ -16,7 +19,11 @@ class Controller:
     """
 
     def __init__(
-        self, challenge_name: str, miner_docker_images: List[str], uids: List[int], challenge_info: Dict
+        self,
+        challenge_name: str,
+        miner_docker_images: List[str],
+        uids: List[int],
+        challenge_info: Dict,
     ):
         """
         Initializes the Controller with the name of the challenge and the list of miner Docker images.
@@ -60,11 +67,11 @@ class Controller:
         self._remove_challenge_container()
         self._create_network(self.local_network)
 
-        
-
         container = self._run_challenge_container()
         bt.logging.info(f"[Controller] Challenge container started: {container.status}")
-        while not self._check_alive(port=constants.CHALLENGE_DOCKER_PORT):
+        while not self._check_alive(
+            port=constants.CHALLENGE_DOCKER_PORT, is_challenger=True
+        ):
             bt.logging.info("[Controller] Waiting for challenge container to start.")
             time.sleep(1)
 
@@ -83,27 +90,43 @@ class Controller:
             docker_run_start_time = time.time()
             kwargs = {}
             if self.resource_limits.get("cuda_device_ids") is not None:
-                kwargs["device_requests"] = [docker.types.DeviceRequest(device_ids=self.resource_limits["cuda_device_ids"], capabilities=[['gpu']])]
+                kwargs["device_requests"] = [
+                    docker.types.DeviceRequest(
+                        device_ids=self.resource_limits["cuda_device_ids"],
+                        capabilities=[["gpu"]],
+                    )
+                ]
             miner_container = self.docker_client.containers.run(
                 miner_docker_image,
                 detach=True,
                 cpu_count=self.resource_limits.get("num_cpus", 2),
                 mem_limit=self.resource_limits.get("mem_limit", "1g"),
-                environment={"CHALLENGE_NAME": self.challenge_name, **self.challenge_info.get("enviroment", {})},
+                environment={
+                    "CHALLENGE_NAME": self.challenge_name,
+                    **self.challenge_info.get("enviroment", {}),
+                },
                 ports={
                     f"{constants.MINER_DOCKER_PORT}/tcp": constants.MINER_DOCKER_PORT
                 },
                 network=self.local_network,
-                **kwargs           
+                **kwargs,
             )
-            while not self._check_alive(port=constants.MINER_DOCKER_PORT) and time.time() - docker_run_start_time < self.challenge_info.get("docker_run_timeout", 600):
+            while not self._check_alive(
+                port=constants.MINER_DOCKER_PORT, is_challenger=False
+            ) and time.time() - docker_run_start_time < self.challenge_info.get(
+                "docker_run_timeout", 600
+            ):
                 bt.logging.info(
                     f"[Controller] Waiting for miner container to start. {miner_container.status}"
                 )
                 time.sleep(1)
-            for miner_input in challenges: 
+            for miner_input in challenges:
                 miner_output = self._submit_challenge_to_miner(miner_input)
-                score = self._score_challenge(miner_input, miner_output) if miner_output is not None else 0
+                score = (
+                    self._score_challenge(miner_input, miner_output)
+                    if miner_output is not None
+                    else 0
+                )
                 logs.append(
                     {
                         "miner_input": miner_input,
@@ -156,6 +179,14 @@ class Controller:
         Runs the Docker container for the challenge using the built image.
         The container runs in detached mode and listens on the port defined by constants.
         """
+
+        kwargs = {}
+        if "same_network" in self.challenge_info:
+            kwargs["network"] = self.local_network
+
+        if "hostname" in self.challenge_info:
+            kwargs["hostname"] = self.challenge_info["hostname"]
+
         container = self.docker_client.containers.run(
             self.challenge_name,
             detach=True,
@@ -163,6 +194,7 @@ class Controller:
                 f"{constants.CHALLENGE_DOCKER_PORT}/tcp": constants.CHALLENGE_DOCKER_PORT
             },
             name=self.challenge_name,
+            **kwargs,
         )
         bt.logging.info(container)
         return container
@@ -195,9 +227,11 @@ class Controller:
         for key in exclude_miner_input_key:
             miner_input[key] = None
         try:
+            _protocol, _ssl_verify = self._check_protocol(is_challenger=False)
             response = requests.post(
-                f"http://localhost:{constants.MINER_DOCKER_PORT}/solve",
+                f"{_protocol}://localhost:{constants.MINER_DOCKER_PORT}/solve",
                 timeout=self.challenge_info.get("challenge_solve_timeout", 60),
+                verify=_ssl_verify,
                 json=miner_input,
             )
             return response.json()
@@ -205,19 +239,25 @@ class Controller:
             bt.logging.error(f"Submit challenge to miner failed: {str(ex)}")
             return None
 
-    def _check_alive(self, port=10001) -> bool:
+    def _check_alive(self, port=10001, is_challenger=True) -> bool:
         """
         Checks if the challenge container is still running.
         """
+
+        _protocol, _ssl_verify = self._check_protocol(is_challenger=is_challenger)
+
         try:
-            response = requests.get(f"http://localhost:{port}/health")
+            response = requests.get(
+                f"{_protocol}://localhost:{port}/health",
+                verify=_ssl_verify,
+            )
             if response.status_code == 200:
                 return True
         except requests.exceptions.ConnectionError:
             return False
         return False
 
-    def _get_challenge_from_container(self, is_check_alive=False) -> dict:
+    def _get_challenge_from_container(self) -> dict:
         """
         Retrieves a challenge input from the running challenge container by making an HTTP POST request.
         The challenge container returns a task that will be sent to the miners.
@@ -225,8 +265,12 @@ class Controller:
         Returns:
             A dictionary representing the challenge input.
         """
+
+        _protocol, _ssl_verify = self._check_protocol(is_challenger=True)
+
         response = requests.get(
-            f"http://localhost:{constants.CHALLENGE_DOCKER_PORT}/task"
+            f"{_protocol}://localhost:{constants.CHALLENGE_DOCKER_PORT}/task",
+            verify=_ssl_verify,
         )
         return response.json()
 
@@ -242,6 +286,9 @@ class Controller:
         Returns:
             A float representing the score for the miner's solution.
         """
+
+        _protocol, _ssl_verify = self._check_protocol(is_challenger=True)
+
         try:
             payload = {
                 "miner_input": miner_input,
@@ -249,13 +296,15 @@ class Controller:
             }
             bt.logging.debug(f"[Controller] Scoring payload: {str(payload)[:100]}...")
             response = requests.post(
-                f"http://localhost:{constants.CHALLENGE_DOCKER_PORT}/score",
+                f"{_protocol}://localhost:{constants.CHALLENGE_DOCKER_PORT}/score",
+                verify=_ssl_verify,
                 json=payload,
             )
             return response.json()
         except Exception as ex:
             bt.logging.error(f"Score challenge failed: {str(ex)}")
             return 0
+
     def _create_network(self, network_name):
         try:
             networks = self.docker_client.networks.list(names=[network_name])
@@ -269,16 +318,36 @@ class Controller:
                 bt.logging.info(f"Network '{network_name}' already exists.")
                 network = self.docker_client.networks.get(network_name)
 
-
             network_info = self.docker_client.api.inspect_network(network.id)
-            subnet = network_info['IPAM']['Config'][0]['Subnet']
+            subnet = network_info["IPAM"]["Config"][0]["Subnet"]
             iptables_commands = [
                 # Block forwarded traffic to the internet
-                ["iptables", "-I", "FORWARD", "-s", subnet, "!", "-d", subnet, "-j", "DROP"],
+                [
+                    "iptables",
+                    "-I",
+                    "FORWARD",
+                    "-s",
+                    subnet,
+                    "!",
+                    "-d",
+                    subnet,
+                    "-j",
+                    "DROP",
+                ],
                 # Prevent NAT to the internet
-                ["iptables", "-t", "nat", "-I", "POSTROUTING", "-s", subnet, "-j", "RETURN"]
+                [
+                    "iptables",
+                    "-t",
+                    "nat",
+                    "-I",
+                    "POSTROUTING",
+                    "-s",
+                    subnet,
+                    "-j",
+                    "RETURN",
+                ],
             ]
-            
+
             for cmd in iptables_commands:
                 try:
                     # Try with sudo
@@ -289,13 +358,47 @@ class Controller:
 
         except docker.errors.APIError as e:
             bt.logging.error(f"Failed to create network: {e}")
-            
-    
+
     def _validate_image_with_digest(self, image):
         """Validate that the provided Docker image includes a SHA256 digest."""
         digest_pattern = r".+@sha256:[a-fA-F0-9]{64}$"  # Regex for SHA256 digest format
         if not re.match(digest_pattern, image):
-            bt.logging.error(f"Invalid image format: {image}. Must include a SHA256 digest. Skip evaluation!")
+            bt.logging.error(
+                f"Invalid image format: {image}. Must include a SHA256 digest. Skip evaluation!"
+            )
             return False
         return True
- 
+
+    def _check_protocol(
+        self, is_challenger: bool = True
+    ) -> Tuple[str, Union[bool, None]]:
+        """Check the protocol scheme and SSL/TLS verification for the challenger or miner.
+
+        Args:
+            is_challenger (bool, optional): Flag to check the protocol for the challenger or miner. Defaults to True.
+
+        Returns:
+            Tuple[str, Union[bool, None]]: A tuple containing the protocol scheme and SSL/TLS verification.
+        """
+
+        _protocol = "http"
+        _ssl_verify: Union[bool, None] = None
+
+        if "protocols" in self.challenge_info:
+            _protocols = self.challenge_info["protocols"]
+
+            if is_challenger:
+                if "challenger" in _protocols:
+                    _protocol = _protocols["challenger"]
+
+                if "challenger_ssl_verify" in _protocols:
+                    _ssl_verify = _protocols["challenger_ssl_verify"]
+
+            if not is_challenger:
+                if "miner" in _protocols:
+                    _protocol = _protocols["miner"]
+
+                if "miner_ssl_verify" in _protocols:
+                    _ssl_verify = _protocols["miner_ssl_verify"]
+
+        return _protocol, _ssl_verify
